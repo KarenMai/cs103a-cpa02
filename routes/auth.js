3c7e95e294f005a49dd9bf2413bde972aaee33fd
Karen Mai <karenmai@Karens-MacBook-Pro.local>
2022-04-26 20:00:44 -0400

    Updating package

--- routes/auth.js
+++ routes/auth.js
@@ -16,7 +16,7 @@
 
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 const User = require('../models/User')
 
